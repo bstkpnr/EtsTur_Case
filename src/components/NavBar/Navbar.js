bc621a6f826f8de0a3f36ec1c5f61c7b9ad14b48
Beste Tokpınar <55047664+bstkpnr@users.noreply.github.com>
2022-06-20 12:53:20 +0300

    create navbar and edit style

---
+++ src/components/NavBar/Navbar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import '../styles/style.scss';
+
+export default function Navbar() {
+    return (
+      <nav className='navigation'>
+        <a href='/'>ETKİNLİK BUL</a>
+        <div className='nav-menu'>
+            <ul>
+                <li>
+                    <a href='/music'>Müzik</a>
+                </li>
+                <li>
+                    <a href='/stage'>Sahne</a>
+                </li>
+                <li>
+                    <a href='/sport'>Spor</a>
+                </li>
+                <li>
+                    <a href='/education'>Eğitim ve Daha Fazlası</a>
+                </li>
+            </ul>
+
+        </div>
+      </nav>
+      )
+  }
