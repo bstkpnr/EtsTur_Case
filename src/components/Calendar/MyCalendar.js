4e1dcc08bc2e9a96dfd254824048e91a4fb2f6e7
Beste Tokpınar <55047664+bstkpnr@users.noreply.github.com>
2022-07-05 00:26:43 +0300

    edit calendar

--- src/components/Calendar/MyCalendar.js
+++ src/components/Calendar/MyCalendar.js
@@ -1,13 +1,37 @@
-import React, { useState } from 'react'
-import Calendar from 'react-calendar'
+import React, { useState } from "react";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
 
 export default function MyCalendar() {
-    const [date, setDate]=useState(new Date());
-    const onDateChange=(newDate)=>{
-        setDate(newDate);
-        console.log(newDate);
-    }
+  const [date, setDate] = useState(new Date());
+  const [showTime, setShowTime] = useState(false);
+
   return (
-    <Calendar onChange={onDateChange} value={date} showNeighboringMonth={false} locale={"en-US"} />
-  )
+    <div>
+      <div>
+        <Calendar
+          onChange={setDate}
+          value={date}
+          onClickDay={() => setShowTime(true)}
+          selectRange={true}
+          locale={"en-US"}
+        />
+      </div>
+
+      {date.length > 0 ? (
+        <p>
+          <span>Start:</span>
+          {date[0].toDateString()}
+          &nbsp; &nbsp;
+          <span>End:</span>
+          {date[1].toDateString()}
+        </p>
+      ) : (
+        <p>
+          <span>Default selected date:</span>
+          {date.toDateString()}
+        </p>
+      )}
+    </div>
+  );
 }
